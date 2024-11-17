eddebbe8441ef744434530ec6ce93bab736fba2e
akachasalma <akachasalma.sa@gmail.com>
2024-11-17 19:55:29 +0100

    config user-service

--- user-service/src/test/java/com/user_service/user_service/UserServiceApplicationTests.java
+++ user-service/src/test/java/com/user_service/user_service/UserServiceApplicationTests.java
@@ -1,9 +1,11 @@
-package com.roadmates.user_service;
+package com.user_service.user_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = "spring.cloud.config.enabled=false")
 class UserServiceApplicationTests {
 
 	@Test
